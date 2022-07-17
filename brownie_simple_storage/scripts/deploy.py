a9a25d0249f6fd6bf1e345de0c465f18c947fee7
Carl Thomas <cthomas4293@hotmail.com>
2022-07-17 18:42:18 -0400

    fixed issue with contract

--- brownie_simple_storage/scripts/deploy.py
+++ brownie_simple_storage/scripts/deploy.py
@@ -1,25 +1,23 @@
-from brownie import accounts, SimpleStorage, network
+from brownie import accounts, SimpleStorage, network, config
 import os
 
 
 def deploy_simple_storage():
-    account = accounts.add(os.getenv("PRIVATE_KEY"))
-    print(account)
-    # account = get_account()
-    # simple_storage = SimpleStorage.deploy({"from": account})
-    # stored_value = simple_storage.retrieve()
-    # print(stored_value)
-    # transaction = simple_storage.store(15, {"from": account})
-    # transaction.wait(1)
-    # updated_stored_value = simple_storage.retrieve()
-    # print(updated_stored_value)
+    account = get_account()
+    simple_storage = SimpleStorage.deploy({"from": account})
+    stored_value = simple_storage.retrieve()
+    print(stored_value)
+    transaction = simple_storage.store(15, {"from": account})
+    transaction.wait(1)
+    updated_stored_value = simple_storage.retrieve()
+    print(updated_stored_value)
 
 
-# def get_account():
-#     if network.show_active() == "development":
-#         return accounts[0]
-#     else:
-#         return accounts.add(os.getenv("WEB3_INFURA_PROJECT_ID"))
+def get_account():
+    if network.show_active() == "development":
+        return accounts[0]
+    else:
+        return accounts.add(config["wallets"]["from_key"])
 
 
 def main():
